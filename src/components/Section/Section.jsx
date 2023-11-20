99813e542df741619ce86c14bfae3c80872101c5
Velms <78916991+Velms@users.noreply.github.com>
2023-11-20 14:04:48 +0200

    1.0

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,17 +1,16 @@
-import PropTypes from "prop-types";
-import css from "./Section.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SectionWrap, SectionTitle } from './Section.styled';
 
-export default function Section({ title, children }) {
+export const Section = ({ title, children }) => {
   return (
-    <section className={css.section}>
-      <div className={css.container}>
-        {title && <h2 className={css.title}>{title}</h2>}
-        {children}
-      </div>
-    </section>
+    <SectionWrap>
+      <SectionTitle>{title}</SectionTitle>
+      {children}
+    </SectionWrap>
   );
-}
+};
 
-Section.prototype = {
-  title: PropTypes.string,
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,41 +1,29 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import { getRandomHexColor } from './getRandomHexColor';
-import {
-  StatisticsSection,
-  Title,
-  StatList,
-  StatItem,
-  Label,
-  Percentage,
-} from './Statistics.styled';
+import { List } from './Statistics.styled';
 
-const Statistics = ({ stats }) => {
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
   return (
-    <StatisticsSection>
-      <Title>Upload stats</Title>
-      <StatList>
-        {stats.map(stat => (
-          <StatItem
-            key={stat.id}
-            style={{ backgroundColor: getRandomHexColor() }}
-          >
-            <Label>{stat.label}</Label>
-            <Percentage>{stat.percentage}%</Percentage>
-          </StatItem>
-        ))}
-      </StatList>
-    </StatisticsSection>
+    <List>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li>Positive Feedback: {positivePercentage}%</li>
+    </List>
   );
 };
 
 Statistics.propTypes = {
-  stats: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })
-  ).isRequired,
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
-
-export default Statistics;
